db639021bff41a2001a2b76e04c05270f70d4cbf
t-a-yokohama <ict.engineer@ayuki.yokohama>
2024-06-30 12:56:16 +0900

    feat: ログイン処理を実装

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildModeration,
+    ],
+});
+
+client.login(process.env.TOKEN);
