4f6323c38e2b3d49877ce4654ee6ad163d1a9ba2
t-a-yokohama <ict.engineer@ayuki.yokohama>
2024-07-06 21:51:06 +0900

    refactor: コンソール出力のタイムスタンプを一箇所に集約

--- src/index.ts
+++ src/index.ts
@@ -8,37 +8,25 @@ import { CustomId } from './enum/customId';
 dotenv.config();
 
 const customConsole = {
-    log: (log: String) =>  {
-        console.log(
-            (new Date)
-                .toLocaleString(
-                    "ja-JP",
-                    { 
-                        year: 'numeric',
-                        month: '2-digit',
-                        day: '2-digit',
-                        hour: '2-digit',
-                        minute: '2-digit',
-                        second: '2-digit'
-                    }
-                ), log
-        );
+    timestamp: function(): String {
+        return (new Date)
+            .toLocaleString(
+                "ja-JP",
+                { 
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit'
+                }
+            );
     },
-    error: (error: any) => {
-        console.error(
-            (new Date)
-                .toLocaleString(
-                    "ja-JP",
-                    { 
-                        year: 'numeric',
-                        month: '2-digit',
-                        day: '2-digit',
-                        hour: '2-digit',
-                        minute: '2-digit',
-                        second: '2-digit'
-                    }
-                ), error
-        );
+    log: function(log: String) {
+        console.log(this.timestamp(), log);
+    },
+    error: function(error: any) {
+        console.error(this.timestamp, error);
     }
 };
 
